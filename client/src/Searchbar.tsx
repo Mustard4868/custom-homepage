d03dadb173338e8edd8b974797122e6eaf4de591
mustard4868 <info@mstrd.eu>
2024-09-19 21:36:39 +0200

    prepare for deploy

--- client/src/Searchbar.tsx
+++ client/src/Searchbar.tsx
@@ -4,20 +4,20 @@ function Searchbar() {
     const [squery, setSquery] = useState('');
     return (
         <form onSubmit={handleSubmit} onChange={handleChange} action="" method="get" className='w-screen flex flex-col items-center text-cat-macchiato-text'>
-            <input type="search" name="" id="squery" placeholder='Search with DuckDuckGo' className='p-2 placeholder-cat-macchiato-text w-1/3 h-10 box-border border-2 border-cat-macchiato-crust rounded-full bg-cat-macchiato-crust bg-opacity-55 hover:scale-105 ease-in-out duration-300' autoComplete="off" />
+            <input type="search" name="" id="squery" placeholder='Search with DuckDuckGo' className='p-2 placeholder-cat-macchiato-text w-1/3 h-10 box-border border-2 border-cat-macchiato-pink rounded-full bg-cat-macchiato-crust bg-opacity-55 hover:scale-105 ease-in-out duration-300' autoComplete="off" />
         </form>
     )
     function handleChange(event: any) {
         setSquery(event.target.value as string);
     }
-}
-
-function handleSubmit(event: any) {
-    event.preventDefault()
-    const squery = (document.getElementById('squery') as HTMLInputElement)?.value;
-    if (squery !== "") {
-        window.location.href = `https://duckduckgo.com/?t=ffab&q=${squery}&ia=web`;
+    function handleSubmit(event: any) {
+        event.preventDefault()
+        if (squery !== "") {
+            window.location.href = `https://duckduckgo.com/?t=ffab&q=${squery}&ia=web`;
+        }
     }
 }
+
+
 
 export default Searchbar
--- client/src/Shortcut.tsx
+++ client/src/Shortcut.tsx
@@ -4,8 +4,8 @@ interface ShortcutProps {
 
 function Shortcut(props: ShortcutProps) {
     return (
-        <form action={props.url} className="pl-1 pr-1 pt-2 hover:scale-110 duration-300 ease-in-out">
-            <input type="image" src={`https://s2.googleusercontent.com/s2/favicons?domain_url=${props.url}`} value="" className="p-1 h-16 w-16 border-2 border-cat-macchiato-green bg-cat-macchiato-crust bg-opacity-50 text-white rounded-xl" />
+        <form action={props.url} className="pl-2 pr-2 pt-4 hover:scale-125 duration-250 ease-in-out">
+            <input type="image" alt={`shortcut ${props.url}`} src={`${props.url}/favicon.ico`} value="" className="p-1 h-16 w-16 border-2 border-cat-macchiato-pink bg-cat-macchiato-crust bg-opacity-50 text-white rounded-xl" />
         </form>
     )
 }
